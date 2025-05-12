071d394d5da6ba5a111834a62925a44f6d55e5dd
overlord66-6 <ahsuw892@gmail.com>
2025-05-12 17:12:28 -0600

    Update Script the best!!.lua

--- Script the best!!.lua
+++ Script the best!!.lua
@@ -1,113 +1,236 @@
--- Load Redz Library
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
+---library1----
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
 
--- Create Window
 local Window = redzlib:MakeWindow({
   Title = "redz Hub : Blox Fruits",
   SubTitle = "by redz9999",
   SaveFolder = "testando | redz lib v5.lua"
 })
-
--- Add Minimize Button
-Window:AddMinimizeButton({
-  Button = { Image = "rbxassetid://71014873973869", BackgroundTransparency = 0 },
-  Corner = { CornerRadius = UDim.new(35, 1) },
-})
-
--- Set Theme
-redzlib:SetTheme("Dark")
-
--- Create Tab
 local Tab1 = Window:MakeTab({"Um", "cherry"})
+local Tab2 = Window:MakeTab({"Dois", "swords"})
+local Tab3 = Window:MakeTab({"Três", "user"})
 
--- Select Tab
-Window:SelectTab(Tab1)
+Tab1:AddButton({"Dark Theme", function()
+  redzlib:SetTheme("Dark")
+end})
 
--- Add Discord Invite
-Tab1:AddDiscordInvite({
-  Name = "Name Hub",
-  Description = "Join server",
-  Logo = "rbxassetid://18751483361",
-  Invite = "Link discord invite",
+Tab1:AddButton({"Darker Theme", function()
+  redzlib:SetTheme("Darker")
+end})
+
+Tab1:AddButton({"Dark Purple", function()
+  redzlib:SetTheme("Purple")
+end})
+
+Window:SelectTab(Tab2)
+local Section = Tab2:AddSection({"Section"})
+local Paragraph = Tab2:AddParagraph({"Paragraph", "This is a Paragraph\nSecond Line"})
+
+local Number = 0
+local Button = Tab2:AddButton({"Button", function()
+  Number = Number + 1
+  Section:Set("Number : " .. tostring(Number))
+  local Dialog = Window:Dialog({
+    Title = "Dialog",
+    Text = "This is a Dialog",
+    Options = {
+      {"Confirm", function()
+        
+      end},
+      {"Maybe", function()
+        
+      end},
+      {"Cancel", function()
+        
+      end}
+    }
+  })
+end})
+
+local Button = Tab2:AddButton({
+  Name = "Invisible Toggle",
+  Description = "Makes the Toggles Invisible"
 })
 
--- Add Section
-local Section = Tab1:AddSection({"Section"})
-
--- Add Paragraph
-local Paragraph = Tab1:AddParagraph({
-  "Paragraph",
-  "This is a Paragraph\nSecond Line"
-})
-
--- Add Button
-Tab1:AddButton({
-  "Print",
-  function()
-    print("Hello World!")
-  end
-})
-
--- Add First Toggle
-local Toggle1 = Tab1:AddToggle({
-  Name = "Toggle",
+local Toggle1 = Tab2:AddToggle({
+  Name = "Toggle 1",
   Description = "This is a <font color='rgb(88, 101, 242)'>Toggle</font> Example",
-  Default = false 
+  Default = false
 })
+
+local Toggle2 = Tab2:AddToggle({
+  Name = "Toggle 2",
+  Default = true
+})
+
+Button:Callback(Toggle1.Visible)
+Button:Callback(Toggle2.Visible)
+
 Toggle1:Callback(function(Value)
-  -- Your logic here
+  Toggle2:Set(false)
+end)
+Toggle2:Callback(function(Value)
+  Toggle1:Set(false)
 end)
 
--- Add Second Toggle
-Tab1:AddToggle({
-  Name = "Toggle",
-  Default = false,
-  Callback = function(v)
-    -- Your logic here
-  end
-})
-
--- Add Slider
-Tab1:AddSlider({
-  Name = "Speed",
+Tab2:AddSlider({
+  Name = "Slider",
   Min = 1,
-  Max = 100,
+  Max = 10,
   Increase = 1,
-  Default = 16,
+  Default = 5,
   Callback = function(Value)
-    -- Your logic here
+    
   end
 })
 
--- Add Dropdown
-local Dropdown = Tab1:AddDropdown({
+
+ local Button = Tab2:AddButton({"Refresh Dropdown"})
+
+local Dropdown = Tab2:AddDropdown({
   Name = "Players List",
   Description = "Select the <font color='rgb(88, 101, 242)'>Number</font>",
   Options = {"one", "two", "three"},
   Default = "two",
   Flag = "dropdown teste",
   Callback = function(Value)
-    -- Your logic here
+    
   end
 })
 
--- Add TextBox
-Tab1:AddTextBox({
-  Name = "Name item",
-  Description = "1 Item on 1 Server", 
-  PlaceholderText = "item only",
-  Callback = function(Value)
-    -- Your logic here
-  end
-})
 
--- Add Dialog
-local Dialog = Window:Dialog({
-  Title = "Dialog",
-  Text = "This is a Dialog",
-  Options = {
-    {"Confirm", function() end},
-    {"Maybe", function() end},
-    {"Cancel", function() end}
+
+-----library2----
+
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/REDzHUB/LibraryV2/main/redzLib")))()
+
+MakeWindow({
+  Hub = {
+    Title = "REDz HUB",
+    Animation = "by : redz9999"
+  },
+  Key = {
+    KeySystem = false,
+    Title = "Key System",
+    Description = "",
+    KeyLink = "",
+    Keys = {"1234"},
+    Notifi = {
+      Notifications = true,
+      CorrectKey = "Running the Script...",
+      Incorrectkey = "The key is incorrect",
+      CopyKeyLink = "Copied to Clipboard"
+    }
   }
+})
+
+
+MinimizeButton({
+  Image = "",
+  Size = {40, 40},
+  Color = Color3.fromRGB(10, 10, 10),
+  Corner = true,
+  Stroke = false,
+  StrokeColor = Color3.fromRGB(255, 0, 0)
+})
+
+local Main = MakeTab({Name = "Main"})
+
+
+
+MakeNotifi({
+  Title = "REDz HUB",
+  Text = "Notificação teste",
+  Time = 5
+})
+
+local section = AddSection(Main, {"Teste"})
+
+
+AddButton(Main, {
+  Name = "Botão teste",
+  Callback = function()
+    
+  end
+})
+
+local Toggle = AddToggle(Main, {
+  Name = "Toggle teste",
+  Default = false,
+  Callback = function(Value)
+    
+  end
+})
+
+
+
+local Slider = AddSlider(Main, {
+  Name = "Slider teste",
+  MinValue = 10,
+  MaxValue = 100,
+  Default = 25,
+  Increase = 1,
+  Callback = function(Value)
+    
+  end
+})
+
+AddKeybind(Main, {
+  Name = "Keybind teste",
+  KeyCode = "E",
+  Default = false,
+  Callback = function(Value)
+    
+  end
+})
+
+
+AddTextBox(Main, {
+  Name = "TextBox teste",
+  Default = "bom dia",
+  PlaceholderText = "teste",
+  ClearText = true,
+  Callback = function(Value)
+    
+  end
+})
+
+
+local Dropdown = AddDropdown(Main, {
+  Name = "Dropdown teste",
+  Options = {"1", "2", "3"},
+  Default = "2",
+  Callback = function(Value)
+    
+  end
+})
+
+
+
+AddColorPicker(Main, {
+  Name = "Color picker teste",
+  Default = Color3.fromRGB(255, 255, 0),
+  Callback = function(Value)
+    
+  end
+})
+
+local Label = AddTextLabel(Main, "AutoFarm")
+
+local Paragraph = AddParagraph(Main, {"Paragraph teste", "bom dia meus manos"})
+
+local Image = AddImageLabel(Main, {
+  Name = "Cool Image",
+  Image = "rbxassetid://"
+})
+
+
+
+local MobileToggle = AddMobileToggle({
+  Name = "Toggle",
+  Visible = true,
+  Callback = function(Value)
+    
+  end
 })
