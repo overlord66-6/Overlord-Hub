5c3ddd833dcf8a8b36e807bd9391dbf6fbe29f77
overlord66-6 <ahsuw892@gmail.com>
2025-05-12 20:40:48 -0600

    Update gamelister.lua

--- gamelister.lua
+++ gamelister.lua
@@ -1,6 +1,6 @@
 local Games = {
 
-[4924922222] = "", -- Brookhaven
+[4924922222] = "https://raw.githubusercontent.com/overlord66-6/Overlord-Hub/refs/heads/main/Brookhaven%20only.lua", -- Brookhaven
 
 }
 
