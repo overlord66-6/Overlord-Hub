d82b195c70185a40f2ab6a1334e0571b6ebb87a8
overlord66-6 <ahsuw892@gmail.com>
2025-05-12 18:02:07 -0600

    Update Script the best.lua

--- Script the best!!.lua
+++ Script the best!!.lua
@@ -28,6 +28,65 @@ do
         Duration = 5 -- Set to nil to make the notification not disappear
     })
 
+
+    -- Input Element for Speed
+local Input = Tabs.Main:AddInput("SpeedInput", {
+    Title = "Speed",
+    Default = "0",  -- Default speed value (0 means no speed adjustment)
+    Placeholder = "Enter speed value",
+    Numeric = true, -- Only allows numbers
+    Finished = true, -- Only calls callback when you press enter
+    Callback = function(Value)
+        print("Input changed:", Value)
+        
+        -- Check if the input is a valid number
+        local speed = tonumber(Value)
+        
+        -- If valid speed and toggle is on, set speed
+        if speed and speed > 0 then
+            -- Enable the toggle and set the speed
+            Toggle:SetValue(true)
+            print("Speed set to:", speed)
+        else
+            -- Disable the toggle when the input is 0 or invalid
+            Toggle:SetValue(false)
+            print("Speed is off or invalid input.")
+        end
+    end
+})
+
+-- Listen for input changes to update speed
+Input:OnChanged(function()
+    print("Input updated:", Input.Value)
+    
+    -- Re-check the input when it changes
+    local speed = tonumber(Input.Value)
+    
+    -- Enable or disable the toggle based on valid speed
+    if speed and speed > 0 then
+        Toggle:SetValue(true)  -- Turn on the toggle if the speed is valid
+    else
+        Toggle:SetValue(false) -- Turn off the toggle if the speed is zero or invalid
+    end
+end)
+
+-- Toggle Element to turn speed on or off
+local Toggle = Tabs.Main:AddToggle("SpeedToggle", {Title = "Enable Speed", Default = false })
+
+-- Listen for toggle changes and adjust speed functionality
+Toggle:OnChanged(function()
+    if Toggle.Value then
+        print("Speed is enabled.")
+        -- Here, you would set the character's speed or any other speed-related functionality.
+    else
+        print("Speed is disabled.")
+        -- You would reset the speed to 0 or stop any movement logic.
+    end
+end)
+
+-- Optionally, you can set the default value of the toggle or speed at start
+Options.SpeedToggle:SetValue(false)
+    
 -- Addons:
 -- SaveManager (Allows you to have a configuration system)
 -- InterfaceManager (Allows you to have a interface managment system)
