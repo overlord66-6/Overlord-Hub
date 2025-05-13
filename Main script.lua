01bc4f3bdd75905594807fdc96008b608b601422
overlord66-6 <ahsuw892@gmail.com>
2025-05-12 20:42:20 -0600

    Update Main script.lua

--- Main script.lua
+++ Main script.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/overlord66-6/Overlord-Hub/refs/heads/main/gamelister.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
